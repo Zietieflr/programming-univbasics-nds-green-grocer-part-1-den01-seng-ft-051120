68bf169bededec53364bb8ab101b2967604fd1b7
Logan McGuire <lrmcguire93@gmail.com>
2020-05-07 23:05:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,13 +9,9 @@ def find_item_by_name_in_collection(name, collection)
   nil; 
 end
 
-#
+#Enumerate through a collection, adding a count and parsing down duplicates 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  
 end
 
 
