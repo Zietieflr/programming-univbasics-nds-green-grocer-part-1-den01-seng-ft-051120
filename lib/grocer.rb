bc4261abf39e4f1a0252aba391e22ada78296b55
Logan McGuire <lrmcguire93@gmail.com>
2020-05-07 23:02:59 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,3 +1,5 @@
+
+#Find item in a collection and return if found, nil if not
 def find_item_by_name_in_collection(name, collection)
   collection.map { |item_hash| 
     if item_hash[:item] == name
@@ -7,6 +9,7 @@ def find_item_by_name_in_collection(name, collection)
   nil; 
 end
 
+#
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
